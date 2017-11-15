4a0966dc59d5525cac46f1807a4aff7eb2d76143
Aurélien Wolz <wolzaurelien@gmail.com>
2017-11-15 15:57:47 +0100

    Add settings storage

--- src/options/options.js
+++ src/options/options.js
@@ -1,1 +1,32 @@
-$('#option-extension-activated').prop('checked',true);
+// Retrieve settings
+var settings = {
+  'activated': true
+}
+
+chrome.storage.sync.get(['settings'], (stored) => {
+  // Check if there is already stored settings
+  if(Object.keys(stored).length != 0) {
+    settings = stored.settings;
+  }
+  // Load settings
+  if(settings.activated) {
+    $('#option-extension-activated').prop('checked',true);
+  } triggerToogleChanges('activated', $('#option-extension-activated'));
+});
+
+// Functions
+function triggerToogleChanges(property, element) {
+  element.change(() => {
+    settings[property] = element.is(":checked");
+    storeSettings();
+  });
+}
+
+function storeSettings() {
+  console.log(settings)
+  chrome.storage.sync.set({
+    'settings': settings
+  }, () => {
+    console.log('Settings updated');
+  });
+}
