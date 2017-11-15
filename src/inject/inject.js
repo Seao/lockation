cae4b462eda5a980e79689dfd03f13a6e7eba552
Aurélien Wolz <wolzaurelien@gmail.com>
2017-11-15 16:32:51 +0100

    Add initial injection script

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,1 +1,45 @@
-console.log("Hello World!");
+// Geolocation API Overriding
+function overrideGeolocation() {
+  if(!navigator.geolocation) return;
+
+  // Current position
+  navigator.geolocation.getCurrentPosition = (success, error, options) => {
+    console.log("called 1");
+  };
+
+  navigator.geolocation.watchPosition = (success, error, options) => {
+  	console.log("called 2");
+  };
+
+  navigator.geolocation.clearWatch = () => {
+  	console.log("called 3");
+  };
+
+  // Clean script injection
+  let script = document.getElementById('lockation');
+	if(script) script.remove();
+}
+
+// Verify extension activation
+getSettings('activated', (value) => {
+  if(value == true) {
+    var inject = "(function(){ " + overrideGeolocation + " overrideGeolocation();})()";
+    var script = document.createElement('script');
+    script.setAttribute('id', 'lockation');
+    script.appendChild(document.createTextNode(inject));
+    document.documentElement.appendChild(script);
+  }
+});
+
+// Functions
+function getSettings(property, callback) {
+  var settings = {
+    "activated": true
+  }
+  chrome.storage.sync.get(['settings'], (stored) => {
+    if(Object.keys(stored).length != 0) {
+      settings = stored.settings;
+    }
+    callback(settings[property]);
+  });
+}
