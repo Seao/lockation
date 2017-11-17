781b80988162fcd59d46a950befa79fb58f613f2
Aurélien Wolz <wolzaurelien@gmail.com>
2017-11-17 21:49:01 +0100

    Add bidirectionnal communication between content and injected scripts

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,5 +1,5 @@
 // Geolocation API Overriding
-function overrideGeolocation() {
+function overrideGeolocation(extension) {
   if(!navigator.geolocation) return;
 
   // Current position
@@ -8,7 +8,22 @@ function overrideGeolocation() {
   };
 
   navigator.geolocation.watchPosition = (success, error, options) => {
-  	console.log("called 2");
+  	// Request content script for noised position
+    window.postMessage({req:'noised'}, '*');
+    window.addEventListener('message', (event) => {
+      if(event.source != window) return;
+      if(event.data.res) {
+        success({
+          coords:{
+            latitude: parseFloat(event.data.res.latitude),
+            longitude: parseFloat(event.data.res.longitude),
+            altitude: 100,
+            accuracy: 10,
+            speed: 0
+          }
+        })
+      }
+    });
   };
 
   navigator.geolocation.clearWatch = () => {
@@ -23,15 +38,20 @@ function overrideGeolocation() {
 // Verify extension activation
 getSettings('activated', (value) => {
   if(value == true) {
-    // Get current position before obfuscation
-    chrome.runtime.sendMessage({req:'geolocation'}, function(response) {
-      console.log(response.position);
-      // Script injection
-      var inject = "(function(){ " + overrideGeolocation + " overrideGeolocation();})()";
-      var script = document.createElement('script');
-      script.setAttribute('id', 'lockation');
-      script.appendChild(document.createTextNode(inject));
-      document.documentElement.appendChild(script);
+    // Script injection
+    var inject = '(function(){ ' + overrideGeolocation + ' overrideGeolocation("' + chrome.runtime.id + '");})()';
+    var script = document.createElement('script');
+    script.setAttribute('id', 'lockation');
+    script.appendChild(document.createTextNode(inject));
+    document.documentElement.appendChild(script);
+    // Event listener for bidirectionnal communication with injected script
+    window.addEventListener('message', (event) => {
+      if(event.source != window) return;
+      if(event.data.req && (event.data.req == 'noised')) {
+        chrome.runtime.sendMessage({req:'geolocation'}, function(response) {
+          window.postMessage({res:response.position}, '*');
+        });
+      }
     });
   }
 });
